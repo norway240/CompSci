097a04c42e44504dcb2508d246caa402099e77ef
norway240 <conman240@gmail.com>
2014-09-11 10:54:29 -0500

    Better GUI layout

--- src/compsci/Main.java
+++ src/compsci/Main.java
@@ -1,7 +1,11 @@
 package compsci;
 
+import java.awt.Component;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.io.PrintStream;
 
+import javax.swing.AbstractButton;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -11,26 +15,37 @@ import javax.swing.JTextArea;
 import compsci.programs.ProgramList;
 
 public class Main {
+
+	static ButtonActionListener bal = new ButtonActionListener();
+	static JPanel panel = new JPanel(new GridBagLayout());
+	static GridBagConstraints c = new GridBagConstraints();
+	static JButton btn001 = new JButton(Consts.BTN001);
+	static JButton btn002 = new JButton(Consts.BTN002);
+	static JButton btn003 = new JButton(Consts.BTN003);
 	
-	public static ButtonActionListener bal = new ButtonActionListener();
-	public static JFrame frame = new JFrame("Computer Science Shenanigans");
-	public static JPanel panel = new JPanel();
-	public static JButton btn001 = new JButton(Consts.BTN001);
-	public static JButton btn002 = new JButton(Consts.BTN002);
-	public static JButton btn003 = new JButton(Consts.BTN003);
-	public static JTextArea textArea = new JTextArea(10,50);
-	public static JScrollPane scroll = new JScrollPane(textArea);
-	public static PrintStream standardOut;
+	private static void addSomething(Object obj,String type, int x, int y, int width){
+		if(type.equalsIgnoreCase("button")){
+			((AbstractButton) obj).addActionListener(bal.act);
+		}
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.gridx = x;
+		c.gridy = y;
+		c.gridwidth = width;
+		panel.add((Component) obj,c);
+	}
 
 	private static void createAndShowGUI(){
-		btn001.addActionListener(bal.act);
-		btn002.addActionListener(bal.act);
-		btn003.addActionListener(bal.act);
+		JFrame frame = new JFrame("Computer Science Shenanigans");
+		JTextArea textArea = new JTextArea(10,50);
+		JScrollPane scroll = new JScrollPane(textArea);
+		@SuppressWarnings("unused")
+		PrintStream standardOut;
 		
-		panel.add(btn001);
-		panel.add(btn002);
-		panel.add(btn003);
-		panel.add(scroll);
+		addSomething(btn001,"button",0,0,1);
+		addSomething(btn002,"button",1,0,1);
+		addSomething(btn003,"button",2,0,1);
+		addSomething(scroll,"scroll",0,1,4);
+		
 		frame.add(panel);
 		
 		PrintStream printStream = new PrintStream(new CustomOutputStream(textArea));
