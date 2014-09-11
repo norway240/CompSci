98f8d5c3d3182fc7a8f3922e95ff5173c252a821
norway240 <conman240@gmail.com>
2014-09-11 08:18:23 -0500

    Route console log to GUI

---
+++ src/compsci/CustomOutputStream.java
@@ -0,0 +1,20 @@
+package compsci;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+import javax.swing.JTextArea;
+
+public class CustomOutputStream extends OutputStream{
+	private JTextArea textArea;
+	
+	public CustomOutputStream(JTextArea textArea){
+		this.textArea = textArea;
+	}
+	
+	public void write(int b) throws IOException{
+		textArea.append(String.valueOf((char)b));
+		textArea.setCaretPosition(textArea.getDocument().getLength());
+	}
+
+}
--- src/compsci/Main.java
+++ src/compsci/Main.java
@@ -1,8 +1,12 @@
 package compsci;
 
+import java.io.PrintStream;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 
 import compsci.programs.ProgramList;
 
@@ -14,6 +18,9 @@ public class Main {
 	public static JButton btn001 = new JButton(Consts.BTN001);
 	public static JButton btn002 = new JButton(Consts.BTN002);
 	public static JButton btn003 = new JButton(Consts.BTN003);
+	public static JTextArea textArea = new JTextArea(10,50);
+	public static JScrollPane scroll = new JScrollPane(textArea);
+	public static PrintStream standardOut;
 
 	private static void createAndShowGUI(){
 		btn001.addActionListener(bal.act);
@@ -23,7 +30,14 @@ public class Main {
 		panel.add(btn001);
 		panel.add(btn002);
 		panel.add(btn003);
+		panel.add(scroll);
 		frame.add(panel);
+		
+		PrintStream printStream = new PrintStream(new CustomOutputStream(textArea));
+		textArea.setEditable(false);
+		standardOut = System.out;
+		System.setOut(printStream);
+		System.setErr(printStream);
 		
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setSize(600,600);
